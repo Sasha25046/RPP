6ebb27d4a6a3ee52f7d77de71a25cc77b56a2009
Sasha25046 <161481283+Sasha25046@users.noreply.github.com>
2026-05-25 13:17:18 +0300

    .

--- RPP/mpi_sort.c
+++ RPP/mpi_sort.c
@@ -4,114 +4,254 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <mpi.h>
-#include <string.h>
 
-void merge(int* a, int n, int m) {
-    int i = 0, j = m, k = 0;
-    int* x = (int*)malloc(n * sizeof(int));
+int ProcNum = 0;
+int ProcRank = 0;
 
-    while (i < m && j < n) {
-        x[k++] = (a[i] < a[j]) ? a[i++] : a[j++];
+void RandomDataInitialization(int* pArray, int Size) {
+    srand((unsigned int)time(NULL) + ProcRank);
+    for (int i = 0; i < Size; i++)
+        pArray[i] = rand() % 100000;
+}
+
+void PrintArray(const int* pArray, int Size) {
+    int show = (Size > 20) ? 10 : Size;
+    printf("  [");
+    for (int i = 0; i < show; i++)
+        printf("%d%s", pArray[i], (i < show - 1) ? ", " : "");
+    if (Size > 20) {
+        printf(", ..., ");
+        for (int i = Size - 10; i < Size; i++)
+            printf("%d%s", pArray[i], (i < Size - 1) ? ", " : "");
     }
-    while (i < m) x[k++] = a[i++];
-    while (j < n) x[k++] = a[j++];
-
-    memcpy(a, x, n * sizeof(int));
-    free(x);
+    printf("]\n");
 }
 
-void merge_sort_serial(int* a, int n) {
+void Merge(int* a, int n, int m) {
+    if (m <= 0 || m >= n) return;
+    int* tmp = (int*)malloc(n * sizeof(int));
+    int i = 0, j = m, k = 0;
+    while (i < m && j < n)
+        tmp[k++] = (a[i] <= a[j]) ? a[i++] : a[j++];
+    while (i < m) tmp[k++] = a[i++];
+    while (j < n) tmp[k++] = a[j++];
+    memcpy(a, tmp, n * sizeof(int));
+    free(tmp);
+}
+
+void MergeSortSerial(int* a, int n) {
     if (n < 2) return;
-
     int mid = n / 2;
-    merge_sort_serial(a, mid);
-    merge_sort_serial(a + mid, n - mid);
-    merge(a, n, mid);
+    MergeSortSerial(a, mid);
+    MergeSortSerial(a + mid, n - mid);
+    Merge(a, n, mid);
 }
 
-int main(int argc, char** argv) {
-    int rank, size, n = 0;
+void SaveArrayToFile(const int* pResult, int Size, const char* filename) {
+    if (ProcRank != 0) return;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    printf("-> Trying to write to file '%s'...\n", filename);
+    FILE* f = fopen(filename, "w");
+    if (!f) {
+        printf("!!! ERROR: Cannot open file %s for writing!\n", filename);
+        return;
+    }
+    fprintf(f, "Size = %d\n", Size);
+    for (int i = 0; i < Size; i++)
+        fprintf(f, "%d\n", pResult[i]);
+    fclose(f);
+    printf("-> SUCCESS: File '%s' saved (Size: %d).\n", filename, Size);
+}
+
+void ProcessInitialization(int** ppArray, int** ppProcData, int** ppResult,
+    int* pSize, int* pRowNum, int** ppSendCounts, int** ppDispls) {
 
     setvbuf(stdout, NULL, _IONBF, 0);
 
-    if (rank == 0) {
-        printf("Enter array size: ");
-        fflush(stdout);
-        if (scanf("%d", &n) != 1) n = 0;
+    if (ProcRank == 0) {
+        do {
+            printf("\nEnter size of the initial objects (array size): ");
+            if (scanf("%d", pSize) != 1) {
+                printf("Invalid input!\n");
+                MPI_Abort(MPI_COMM_WORLD, 1);
+            }
+            if (*pSize < ProcNum)
+                printf("Size of the objects must be greater than number of processes!\n");
+        } while (*pSize < ProcNum);
     }
 
-    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(pSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    if (n <= 0) {
-        MPI_Finalize();
-        return 0;
+    *ppSendCounts = (int*)malloc(ProcNum * sizeof(int));
+    *ppDispls = (int*)malloc(ProcNum * sizeof(int));
+
+    int RestRows = *pSize;
+    (*ppSendCounts)[0] = RestRows / ProcNum;
+    (*ppDispls)[0] = 0;
+    for (int i = 1; i < ProcNum; i++) {
+        RestRows -= (*ppSendCounts)[i - 1];
+        (*ppSendCounts)[i] = RestRows / (ProcNum - i);
+        (*ppDispls)[i] = (*ppDispls)[i - 1] + (*ppSendCounts)[i - 1];
     }
 
-    int* data = NULL;
-    if (rank == 0) {
-        data = (int*)malloc(n * sizeof(int));
-        srand((unsigned int)time(NULL));
-        for (int i = 0; i < n; i++) data[i] = rand() % 10000;
+    *pRowNum = (*ppSendCounts)[ProcRank];
+    *ppProcData = (int*)malloc(*pRowNum * sizeof(int));
+
+    if (ProcRank == 0) {
+        *ppArray = (int*)malloc(*pSize * sizeof(int));
+        *ppResult = (int*)malloc(*pSize * sizeof(int));
+        RandomDataInitialization(*ppArray, *pSize);
+
+        SaveArrayToFile(*ppArray, *pSize, "initial_data_parallel.txt");
     }
+}
 
-    int* sendcounts = (int*)malloc(size * sizeof(int));
-    int* displs = (int*)malloc(size * sizeof(int));
-    int base = n / size;
-    int rem = n % size;
+void DataDistribution(int* pArray, int* pProcData, int* pSendCounts, int* pDispls, int RowNum) {
+    MPI_Scatterv(pArray, pSendCounts, pDispls, MPI_INT, pProcData, RowNum, MPI_INT, 0, MPI_COMM_WORLD);
+}
 
-    for (int i = 0; i < size; i++) {
-        sendcounts[i] = base + (i < rem ? 1 : 0);
-        displs[i] = (i == 0) ? 0 : displs[i - 1] + sendcounts[i - 1];
+void TestDistribution(int* pArray, int* pProcData, int Size, int RowNum) {
+    if (ProcRank == 0) {
+        printf("Initial Array:\n");
+        PrintArray(pArray, Size);
     }
-
-    int local_n = sendcounts[rank];
-    int* sub_array = (int*)malloc(local_n * sizeof(int));
-
     MPI_Barrier(MPI_COMM_WORLD);
-    double start_mpi = MPI_Wtime();
 
-    MPI_Scatterv(data, sendcounts, displs, MPI_INT, sub_array, local_n, MPI_INT, 0, MPI_COMM_WORLD);
+    for (int i = 0; i < ProcNum; i++) {
+        if (ProcRank == i) {
+            printf("\nProcRank = %d, Local Size = %d\n", ProcRank, RowNum);
+            PrintArray(pProcData, RowNum);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+}
 
-    merge_sort_serial(sub_array, local_n);
-
+void ParallelMergePhase(int** ppProcData, int* pRowNum) {
     int step = 1;
-    while (step < size) {
-        if (rank % (2 * step) == 0) {
-            if (rank + step < size) {
+    while (step < ProcNum) {
+        if (ProcRank % (2 * step) == 0) {
+            int partner = ProcRank + step;
+            if (partner < ProcNum) {
                 int recv_size;
-                MPI_Recv(&recv_size, 1, MPI_INT, rank + step, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                int* recv_data = (int*)malloc(recv_size * sizeof(int));
-                MPI_Recv(recv_data, recv_size, MPI_INT, rank + step, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                int* merged = (int*)malloc((local_n + recv_size) * sizeof(int));
-                memcpy(merged, sub_array, local_n * sizeof(int));
-                memcpy(merged + local_n, recv_data, recv_size * sizeof(int));
-                merge(merged, local_n + recv_size, local_n);
-                free(sub_array); free(recv_data);
-                sub_array = merged;
-                local_n += recv_size;
+                MPI_Recv(&recv_size, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                int* recv_buf = (int*)malloc(recv_size * sizeof(int));
+                MPI_Recv(recv_buf, recv_size, MPI_INT, partner, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                int new_size = *pRowNum + recv_size;
+                int* merged = (int*)malloc(new_size * sizeof(int));
+                memcpy(merged, *ppProcData, *pRowNum * sizeof(int));
+                memcpy(merged + *pRowNum, recv_buf, recv_size * sizeof(int));
+
+                Merge(merged, new_size, *pRowNum);
+
+                free(*ppProcData);
+                free(recv_buf);
+                *ppProcData = merged;
+                *pRowNum = new_size;
             }
         }
         else {
-            int target = rank - step;
-            MPI_Send(&local_n, 1, MPI_INT, target, 0, MPI_COMM_WORLD);
-            MPI_Send(sub_array, local_n, MPI_INT, target, 0, MPI_COMM_WORLD);
+            int target = ProcRank - step;
+            MPI_Send(pRowNum, 1, MPI_INT, target, 0, MPI_COMM_WORLD);
+            MPI_Send(*ppProcData, *pRowNum, MPI_INT, target, 1, MPI_COMM_WORLD);
             break;
         }
         step *= 2;
     }
+}
 
-    if (rank == 0) {
-        printf("MPI (Tree Merge): %f sec\n", MPI_Wtime() - start_mpi);
-        free(data);
+void ResultCollection(int* pProcData, int* pResult, int Size) {
+    if (ProcRank == 0) {
+        memcpy(pResult, pProcData, Size * sizeof(int));
+    }
+}
+
+void TestResult(int* pArray, int* pResult, int Size) {
+    if (ProcRank != 0) return;
+
+    int* pSerial = (int*)malloc(Size * sizeof(int));
+    memcpy(pSerial, pArray, Size * sizeof(int));
+    MergeSortSerial(pSerial, Size);
+
+    int equal = 1;
+    for (int i = 0; i < Size; i++) {
+        if (pResult[i] != pSerial[i]) { equal = 0; break; }
     }
 
-    free(sub_array); free(sendcounts); free(displs);
+    if (equal)
+        printf("\nThe results of serial and parallel algorithms are identical.\n");
+    else
+        printf("\nERROR: The results of serial and parallel algorithms are NOT identical!\n");
+
+    free(pSerial);
+}
+
+void ProcessTermination(int* pArray, int* pProcData, int* pResult, int* pSendCounts, int* pDispls) {
+    if (ProcRank == 0) {
+        free(pArray);
+        free(pResult);
+    }
+    free(pProcData);
+    free(pSendCounts);
+    free(pDispls);
+}
+
+int main(int argc, char* argv[]) {
+    int* pArray = NULL;
+    int* pProcData = NULL;
+    int* pResult = NULL;
+    int* pSendCounts = NULL;
+    int* pDispls = NULL;
+    int   Size = 0;
+    int   RowNum = 0;
+    double Start, Finish, Duration;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+
+    if (ProcRank == 0) {
+        printf("Parallel MPI Merge Sort program\n");
+        printf("Number of available processes = %d\n", ProcNum);
+    }
+
+    ProcessInitialization(&pArray, &pProcData, &pResult, &Size, &RowNum, &pSendCounts, &pDispls);
+
+    if (Size <= 20) {
+        TestDistribution(pArray, pProcData, Size, RowNum);
+    }
+
+    Start = MPI_Wtime();
+
+    DataDistribution(pArray, pProcData, pSendCounts, pDispls, RowNum);
+
+    MergeSortSerial(pProcData, RowNum);
+
+    ParallelMergePhase(&pProcData, &RowNum);
+
+    ResultCollection(pProcData, pResult, Size);
+
+    Finish = MPI_Wtime();
+    Duration = Finish - Start;
+
+    if (ProcRank == 0) {
+        if (Size <= 20) {
+            printf("\n Result Array:\n");
+            PrintArray(pResult, Size);
+        }
+        printf("\n Time of execution: %f sec\n", Duration);
+
+        SaveArrayToFile(pResult, Size, "sorted_result_parallel.txt");
+    }
+
+    TestResult(pArray, pResult, Size);
+
+    ProcessTermination(pArray, pProcData, pResult, pSendCounts, pDispls);
+
     MPI_Finalize();
     return 0;
 }
--- RPP/omp_serial.c
+++ RPP/omp_serial.c
@@ -4,81 +4,203 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <omp.h>
-#include <string.h>
 
-void merge(int* a, int n, int m) {
-    int i = 0, j = m, k = 0;
-    int* x = (int*)malloc(n * sizeof(int));
+void DummyDataInitialization(int* pArray, int Size) {
+    for (int i = 0; i < Size; i++)
+        pArray[i] = Size - i;
+}
 
-    while (i < m && j < n) {
-        x[k++] = (a[i] < a[j]) ? a[i++] : a[j++];
+void RandomDataInitialization(int* pArray, int Size) {
+    srand((unsigned int)time(NULL));
+    for (int i = 0; i < Size; i++)
+        pArray[i] = rand() % 100000;
+}
+
+void PrintArray(const int* pArray, int Size) {
+    int show = (Size > 20) ? 10 : Size;
+    printf("  [");
+    for (int i = 0; i < show; i++)
+        printf("%d%s", pArray[i], (i < show - 1) ? ", " : "");
+    if (Size > 20) {
+        printf(", ..., ");
+        for (int i = Size - 10; i < Size; i++)
+            printf("%d%s", pArray[i], (i < Size - 1) ? ", " : "");
     }
-    while (i < m) x[k++] = a[i++];
-    while (j < n) x[k++] = a[j++];
-
-    memcpy(a, x, n * sizeof(int));
-    free(x);
+    printf("]\n");
 }
 
-void merge_sort_serial(int* a, int n) {
-    if (n < 2) return;
-
-    int mid = n / 2;
-    merge_sort_serial(a, mid);
-    merge_sort_serial(a + mid, n - mid);
-    merge(a, n, mid);
-}
-
-void merge_sort_omp(int* a, int n) {
-    if (n < 2) return;
-
-    if (n < 1000) {
-        merge_sort_serial(a, n);
+void Merge(int* a, int n, int m) {
+    int* tmp = (int*)malloc(n * sizeof(int));
+    if (!tmp) {
+        printf("Error: Memory allocation failed in Merge.\n");
         return;
     }
+    int i = 0, j = m, k = 0;
+    while (i < m && j < n)
+        tmp[k++] = (a[i] <= a[j]) ? a[i++] : a[j++];
+    while (i < m) tmp[k++] = a[i++];
+    while (j < n) tmp[k++] = a[j++];
+    memcpy(a, tmp, n * sizeof(int));
+    free(tmp);
+}
 
+void MergeSortSerial(int* a, int n) {
+    if (n < 2) return;
     int mid = n / 2;
-#pragma omp task shared(a)
-    merge_sort_omp(a, mid);
+    MergeSortSerial(a, mid);
+    MergeSortSerial(a + mid, n - mid);
+    Merge(a, n, mid);
+}
 
-#pragma omp task shared(a)
-    merge_sort_omp(a + mid, n - mid);
+#define OMP_THRESHOLD 2000
+
+void MergeSortOMP(int* a, int n) {
+    if (n < 2) return;
+    if (n < OMP_THRESHOLD) {
+        MergeSortSerial(a, n);
+        return;
+    }
+    int mid = n / 2;
+
+#pragma omp task shared(a) firstprivate(mid)
+    MergeSortOMP(a, mid);
+
+#pragma omp task shared(a) firstprivate(mid, n)
+    MergeSortOMP(a + mid, n - mid);
 
 #pragma omp taskwait
-    merge(a, n, mid);
+    Merge(a, n, mid);
+}
+
+int CheckResult(const int* serial_result, const int* omp_result, int Size) {
+    for (int i = 1; i < Size; i++) {
+        if (serial_result[i] < serial_result[i - 1]) return 0;
+        if (omp_result[i] < omp_result[i - 1]) return 0;
+    }
+    for (int i = 0; i < Size; i++) {
+        if (serial_result[i] != omp_result[i]) return 0;
+    }
+    return 1;
+}
+
+void SaveArrayToFile(const int* pArray, int Size, const char* filename) {
+    FILE* f = fopen(filename, "w");
+    if (!f) {
+        printf("  Cannot open %s for writing.\n", filename);
+        return;
+    }
+    fprintf(f, "Size = %d\n", Size);
+    for (int i = 0; i < Size; i++)
+        fprintf(f, "%d\n", pArray[i]);
+    fclose(f);
+    printf("  Data saved to '%s'\n", filename);
+}
+
+void ProcessInitialization(int** ppSerial, int** ppOMP, int* pSize, int* pNumThreads) {
+    do {
+        printf("\nEnter array size: ");
+        if (scanf("%d", pSize) != 1) {
+            printf("  Invalid input!\n");
+            exit(1);
+        }
+        if (*pSize <= 0) printf("  Array size must be greater than 0!\n");
+    } while (*pSize <= 0);
+
+    int maxHardwareThreads = omp_get_max_threads();
+
+    do {
+        printf("Enter number of OpenMP threads (cores) [1-%d]: ", maxHardwareThreads);
+        if (scanf("%d", pNumThreads) != 1) {
+            printf("  Invalid input!\n");
+            exit(1);
+        }
+        if (*pNumThreads <= 0 || *pNumThreads > maxHardwareThreads) {
+            printf("  Number of threads must be between 1 and %d!\n", maxHardwareThreads);
+        }
+    } while (*pNumThreads <= 0 || *pNumThreads > maxHardwareThreads);
+
+    printf("\nChosen size = %d, threads = %d\n", *pSize, *pNumThreads);
+
+    *ppSerial = (int*)malloc(*pSize * sizeof(int));
+    *ppOMP = (int*)malloc(*pSize * sizeof(int));
+
+    if (!*ppSerial || !*ppOMP) {
+        printf("Error: Memory allocation failed for arrays.\n");
+        exit(1);
+    }
+
+    RandomDataInitialization(*ppSerial, *pSize);
+    SaveArrayToFile(*ppSerial, *pSize, "initial_data_omp.txt");
+    memcpy(*ppOMP, *ppSerial, *pSize * sizeof(int));
+}
+
+void ProcessTermination(int* pSerial, int* pOMP) {
+    free(pSerial);
+    free(pOMP);
 }
 
 int main() {
-    int n = 0;
+    int* pSerial = NULL;
+    int* pOMP = NULL;
+    int   Size = 0;
+    int   NumThreads = 0;
+    double t1, t2;
 
-    printf("Enter array size: ");
-    if (scanf("%d", &n) != 1 || n <= 0) return 0;
+    printf("=== Serial & Parallel OpenMP Merge Sort program ===\n");
 
-    int* data_serial = (int*)malloc(n * sizeof(int));
-    int* data_omp = (int*)malloc(n * sizeof(int));
+    ProcessInitialization(&pSerial, &pOMP, &Size, &NumThreads);
 
-    srand((unsigned int)time(NULL));
-    for (int i = 0; i < n; i++) {
-        int val = rand() % 10000;
-        data_serial[i] = val;
-        data_omp[i] = val;
+    if (Size <= 20) {
+        printf("\nInitial array:\n");
+        PrintArray(pSerial, Size);
     }
 
-    double t1 = omp_get_wtime();
-    merge_sort_serial(data_serial, n);
-    printf("Serial: %f sec\n", omp_get_wtime() - t1);
+    
+    t1 = omp_get_wtime();
+    MergeSortSerial(pSerial, Size);
+    t2 = omp_get_wtime();
+    double serialTime = t2 - t1;
+    printf("\nSerial Merge Sort time : %f sec\n", serialTime);
 
-    double t2 = omp_get_wtime();
+ 
+    omp_set_num_threads(NumThreads);
+
+    t1 = omp_get_wtime();
 #pragma omp parallel
     {
 #pragma omp single
-        merge_sort_omp(data_omp, n);
+        {
+            MergeSortOMP(pOMP, Size);
+        }
     }
-    printf("OpenMP: %f sec\n", omp_get_wtime() - t2);
+    t2 = omp_get_wtime();
+    double ompTime = t2 - t1;
+    printf("OpenMP Merge Sort time : %f sec (threads = %d)\n", ompTime, NumThreads);
 
-    free(data_serial);
-    free(data_omp);
+    if (Size <= 20) {
+        printf("\nOMP result:\n");
+        PrintArray(pOMP, Size);
+    }
+
+    printf("\nSpeedup (serial / OMP): %.2f\n", serialTime / ompTime);
+
+    printf("\nVerifying results... ");
+    if (CheckResult(pSerial, pOMP, Size)) {
+        printf("The results of serial and OpenMP algorithms are identical.\n");
+    }
+    else {
+        printf("ERROR: results mismatch or array not sorted!\n");
+    }
+
+    SaveArrayToFile(pSerial, Size, "sorted_result_serial_omp.txt");
+    ProcessTermination(pSerial, pOMP);
+
+    printf("\nPress Enter to exit...\n");
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+    getchar();
     return 0;
 }
